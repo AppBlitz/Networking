8bec201ede8ec100a044f58b0ec5b1e8e5ddedf3
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-03-10 13:35:49 -0500

    Empezando ejercicio 4

---
+++ four_exercise/app/src/main/java/exercise/EchoTCPClient.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoTCPClient {
+    public static void main(String[] args) throws IOException {
+        Socket clientSideSocket = new Socket("localhost", 3400);
+        PrintWriter toNetwork = new PrintWriter(clientSideSocket.getOutputStream(), true);
+        BufferedReader fromNetwork = new BufferedReader(new
+                InputStreamReader(clientSideSocket.getInputStream()));
+        Scanner scanerBacano=new Scanner(System.in);
+        System.out.print("Ingrese cualquier cosa");
+        String cualquierCosa=scanerBacano.nextLine();
+        toNetwork.println(cualquierCosa);
+        String fromServer = fromNetwork.readLine();
+        System.out.println("[Client] From server: " + fromServer);
+        clientSideSocket.close();
+    }
+}
