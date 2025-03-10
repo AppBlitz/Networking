86944b8217bc437d3b8571af69c2c913520096e0
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-03-09 20:16:32 -0500

    Ejercicio 7 realizado

---
+++ eight_exercise/app/src/main/java/exercise/EchoTCPClient.java
@@ -0,0 +1,22 @@
+package exercise;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoTCPClient {
+    public static void main(String[] args) throws IOException {
+        Socket clientSideSocket = new Socket("localhost", 3400);
+        PrintWriter toNetwork = new PrintWriter(clientSideSocket.getOutputStream(), true);
+        BufferedReader fromNetwork = new BufferedReader(new
+                InputStreamReader(clientSideSocket.getInputStream()));
+        String cualquierCosa=JOptionPane.showInputDialog("Ingrese cualquier cosa para ser enviada al servidor");
+        toNetwork.println(cualquierCosa);
+        String fromServer = fromNetwork.readLine();
+        System.out.println("[Client] From server: " + fromServer);
+        clientSideSocket.close();
+    }
+}
