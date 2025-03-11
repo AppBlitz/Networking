b02ffdb66cf76746740b112c17acc37cef9239aa
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-03-10 21:47:18 -0500

    Se modifico otra vez el ejerccio 4 xd

--- four_exercise/app/src/main/java/exercise/EchoTCPServer.java
+++ four_exercise/app/src/main/java/exercise/EchoTCPServer.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class EchoTCPServer {
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(3400);
         System.out.println("The Echo TCP server is running on port 3400 ...");
-        System.out.println("La direccion Ip del servidor es"+listener.getInetAddress());
+        System.out.println("La direccion Ip del servidor es"+ InetAddress.getLocalHost());
         System.out.println("El puerto local del servidor es "+listener.getLocalPort());
         System.out.println("The server is waiting for a client.");
         Socket serverSideSocket = listener.accept();
