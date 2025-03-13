0c26d7fdcc90b2989de558316468af24c1f85f97
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-03-12 20:59:59 -0500

    Se modificaron las clases del ejercicom 5

--- five_exercise/app/src/main/java/exercise/EchoTCPClient.java
+++ five_exercise/app/src/main/java/exercise/EchoTCPClient.java
@@ -1,12 +1,10 @@
 package exercise;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class EchoTCPClient {
     public static void main(String[] args) throws IOException {
@@ -14,7 +12,7 @@ public class EchoTCPClient {
         PrintWriter toNetwork = new PrintWriter(clientSideSocket.getOutputStream(), true);
         BufferedReader fromNetwork = new BufferedReader(new
                 InputStreamReader(clientSideSocket.getInputStream()));
-        toNetwork.println("Conexion encontrada, enviando mensaje desde el servidor");
+        toNetwork.println("Mensaje enviado desde el servidor");
         String fromServer = fromNetwork.readLine();
         System.out.println("[Client] From server: " + fromServer);
         clientSideSocket.close();
--- five_exercise/app/src/main/java/exercise/EchoTCPServer.java
+++ five_exercise/app/src/main/java/exercise/EchoTCPServer.java
@@ -4,18 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class EchoTCPServer {
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(3400);
         System.out.println("The Echo TCP server is running on port 3400 ...");
+        System.out.println("La direccion Ip del servidor es"+ InetAddress.getLocalHost());
+        System.out.println("El puerto local del servidor es "+listener.getLocalPort());
         System.out.println("The server is waiting for a client.");
         Socket serverSideSocket = listener.accept();
         System.out.println("A client has connected");
-        System.out.println("La direccion Ip del cliente es: "+ serverSideSocket.getInetAddress().getHostAddress());
-        System.out.println("El puerto local al que esta conectado el cliente es: "+serverSideSocket.getLocalPort());
-        System.out.println("El puerto al que esta conectado el cliente es: "+serverSideSocket.getPort());
         BufferedReader fromNetwork = new BufferedReader(new
                 InputStreamReader(serverSideSocket.getInputStream()));
         PrintWriter toNetwork = new PrintWriter(serverSideSocket.getOutputStream(), true);
